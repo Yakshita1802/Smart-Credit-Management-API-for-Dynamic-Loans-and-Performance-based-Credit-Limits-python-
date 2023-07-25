1f1c5d3cd873c27eceafa3fbccaf85518c55c7f5
Yakshita <101899906+Yakshita1802@users.noreply.github.com>
2023-07-25 14:36:17 -0400

    Create service.py

---
+++ service.py
@@ -0,0 +1,44 @@
+from database import get_db_connection
+
+def add_loan():
+    loans = [{"name": "test"}]
+    
+    return loans
+
+def get_loan_by_id(loan_id):
+    
+    loans = {"name": "test"}
+    
+    return loans
+
+def get_loans_by_borrowers_id(borrowers_id):
+    
+    loans = {"name": "test"}
+    
+    return loans
+
+def get_all_loans():
+    
+    loans = {"name": "test"}
+    
+    return loans
+
+def get_credit_limit(borrowers_id):
+    
+    loans = {"name": "test"}
+    
+    return loans
+
+def get_paymentdetails(loan_id):
+    
+    loans = {"name": "test"}
+    
+    return loans
+
+def loan_repayment(loan_id):
+    
+    loans = {"name": "test"}
+    
+    return loans
+
+
